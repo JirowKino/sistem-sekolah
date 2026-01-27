60b27f818c16c1d899834cd4df5019085fb86c31
JirowKino <jobijohanes7@gmial.com>
2026-01-27 08:05:58 +0700

    feat: implement simple router\

--- app/core/Router.php
+++ app/core/Router.php
@@ -6,8 +6,22 @@ class Router
 
     public function run()
     {
-        echo 'Router is running';
+        $method = $_SERVER['REQUEST_METHOD']; 
+        $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+        if ($method == 'GET' && $uri == '/sugus') {
+            echo '<h1>Daftar Sugus</h1>';
+            echo '<p>Menampilkan daftar sugus</p>';
+            return;
+        }
+
+           http_response_code(404);
+           echo '<h1> 404-Page Not Found</h1>';
     }
+
+
+  
+
 
 }
 
