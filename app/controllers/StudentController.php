69692f48ab52d955ab19d903abea21efb19ee3a8
JirowKino <jobijohanes7@gmial.com>
2026-02-24 08:33:53 +0700

    wip: setup tailwind and setup views

--- app/controllers/StudentController.php
+++ app/controllers/StudentController.php
@@ -6,20 +6,17 @@ class StudentController
 
     public function index()
     {
-         echo '<h1>Daftar Sugus</h1>';
-         echo '<p>Menampilkan daftar sugus</p>';
+      require_once './app/views/students/index.php';
     }
 
     public function create()
     {
-         echo '<h1>Tambah Sugus</h1>';
-         echo '<p>Menampilkan form tambah sugus</p>';
+        require_once './app/views/students/create.php';
     }
 
      public function show(string $id)
      {
-          echo '<h1>Detail Sugus</h1>';
-          echo "<p>Menampilakan detail sugus dengan ID: {$id}</p>";
+         require_once './app/views/students/show.php';
      }
 
 }
---
+++ app/views/students/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daftar Sugus</title>
+    <link rel="stylesheet" href="/css/output.css">
+</head>
+
+<body>
+    <h1 class="text-red-500">Daftar Sugus</h1>
+    <p>Menampilkan Daftar Sugus</p>
+</body>
+</html>
